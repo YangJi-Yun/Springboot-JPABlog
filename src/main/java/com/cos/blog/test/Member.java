8ec1b3654299a8226a4192d9b08667a98e870130
YangJi-Yun <88270330+YangJi-Yun@users.noreply.github.com>
2022-02-25 04:19:38 +0900

    http 요청실습

---
+++ src/main/java/com/cos/blog/test/Member.java
@@ -0,0 +1,42 @@
+package com.cos.blog.test;
+
+public class Member {
+	
+	// 변수의 상태는 메서드에 의해서 변경이 되게 설계해야 한다.
+	private int id;
+	private String username;
+	private String password;
+	private String email;
+	
+	
+	public Member(int id, String username, String password, String email) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
