e381c8a71e0599284e10f9714557d44f2fe5ffad
YangJi-Yun <88270330+YangJi-Yun@users.noreply.github.com>
2022-02-26 00:44:28 +0900

    lombok Test

--- src/main/java/com/cos/blog/test/HttpcontrollerTest.java
+++ src/main/java/com/cos/blog/test/HttpcontrollerTest.java
@@ -15,6 +15,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpcontrollerTest {
 	
+	private static final String TAG="HttpControllerTest : ";
+	
+	//lombokTest1
+//	@GetMapping("/http/lombok")
+//	public String lombokTest() {
+//		Member m = new Member(1, "cos", "1234", "cos@nate.com");
+//		System.out.println(TAG+"getter : "+m.getId());
+//		m.setId(5000);
+//		System.out.println(TAG+"setter : "+m.getId());
+//		return "lombok test1 완료";
+//	}
+	
+	//lombokTest2
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("cos").password("1234").email("cos@nate.com").build();
+		System.out.println(TAG+"getter : "+m.getId());
+		System.out.println(TAG+"getter : "+m.getUsername());
+		m.setId(5000);
+		m.setUsername("ssar");
+		System.out.println(TAG+"setter : "+m.getId());
+		System.out.println(TAG+"setter : "+m.getUsername());
+		return "lombok test2 완료";
+	}
+	//builder 패턴을 쓰면 오버로딩을 하지 않아도 된다. (다른 개수의 파라미터를 가지는 생성자가 없어도)   
+	//builder 패턴을 쓰면 생성자의 순서를 지키지 않아도 된다.
+	
+		
 	//인터넷 브라우저 요청은 무조건 get방식밖에 할 수 없다.
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,5 +1,13 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+//@AllArgsConstructor //lombokTest1
+@NoArgsConstructor //빈생성자
 public class Member {
 	
 	// 변수의 상태는 메서드에 의해서 변경이 되게 설계해야 한다.
@@ -8,35 +16,22 @@ public class Member {
 	private String password;
 	private String email;
 	
-	
+	//lomboktest2
+	@Builder
 	public Member(int id, String username, String password, String email) {
 		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.email = email;
 	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
+	
+	
+	
 }
+
+//@Getter
+//@Setter
+//@Data //getter, setter 동시
+//@AllArgsConstructor // 생성자
+//@RequiredArgsConstructor//불변성 유지를 위해 final 붙은 변수들의 생성자 
