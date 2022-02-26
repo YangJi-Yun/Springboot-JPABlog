a3ea5da3e1dcb8a247cd8339614376f2ab40102f
YangJi-Yun <88270330+YangJi-Yun@users.noreply.github.com>
2022-02-27 03:28:38 +0900

    http 요청실습

--- src/main/java/com/cos/blog/http/HttpcontrollerTest.java
+++ src/main/java/com/cos/blog/http/HttpcontrollerTest.java
@@ -1,4 +1,4 @@
-package com.cos.blog.test;
+package com.cos.blog.http;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,36 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 //사용자 요청 -> 응답(Data)
 @RestController
 public class HttpcontrollerTest {
-	
-	private static final String TAG="HttpControllerTest : ";
-	
-	//http://localhost:8080/
-	//lombokTest1
-	@GetMapping("/http/lombok")
-	public String lombokTest() {
-		Member m = new Member(1, "cos", "1234", "cos@nate.com");
-		System.out.println(TAG+"getter : "+m.getId());
-		m.setId(5000);
-		System.out.println(TAG+"setter : "+m.getId());
-		return "lombok test1 완료";
-	}
-	
-	//lombokTest2
-//	@GetMapping("/http/lombok")
-//	public String lombokTest() {
-//		Member m = Member.builder().username("cos").password("1234").email("cos@nate.com").build();
-//		System.out.println(TAG+"getter : "+m.getId());
-//		System.out.println(TAG+"getter : "+m.getUsername());
-//		m.setId(5000);
-//		m.setUsername("ssar");
-//		System.out.println(TAG+"setter : "+m.getId());
-//		System.out.println(TAG+"setter : "+m.getUsername());
-//		return "lombok test2 완료";
-//	}
-	//builder 패턴을 쓰면 오버로딩을 하지 않아도 된다. (다른 개수의 파라미터를 가지는 생성자가 없어도)   
-	//builder 패턴을 쓰면 생성자의 순서를 지키지 않아도 된다.
-	
-		
+			
 	//인터넷 브라우저 요청은 무조건 get방식밖에 할 수 없다.
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
@@ -50,10 +21,6 @@ public class HttpcontrollerTest {
 		return "get 요청 :"+m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 		
-	//	public String getTest(@RequestParam int id, @RequestParam String username) {
-	//	return "get 요청 :"+id+", "+username;
-	//}
-	
 	//http://localhost:8080/http/post (insert)
 	@PostMapping("/http/post")
 		public String postTest(Member m) {//MIME 타입 : x-www-form=urlencoded = <form><input type=""></form> //MessageConverter (스프링부트) 오브젝트에 맵핑 시켜줌
