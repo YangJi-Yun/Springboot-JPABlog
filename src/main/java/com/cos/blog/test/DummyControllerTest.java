f1440137f4517099936da1b4ab25a42403a2adaf
YangJi-Yun <88270330+YangJi-Yun@users.noreply.github.com>
2022-03-16 00:37:18 +0900

    id로 select 테스트

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +22,49 @@ public class DummyControllerTest {
 	// UserRepository 인터페이스가 이미 메모리에 떠있다.
 	private UserRepository userRepository;
 	//스프링이 @RestController을 읽어서 DummyControllerTest를 메모리에 띄어줄 때는 null이다.
+	
+	// {id} 주소로 파라미터를 전달받을 수 있음
+	// http://localhost:8000/blog/dummy/user/3
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) {
+		
+		// user=4를 찾으면 내가 데이터베이스에서 못찾아오게 되면 user가 null이 된다.
+		// null 이  return되면 프로그램에 문제가 있다
+		// Optional로 너의 User 객체를 감싸서 가져올테니 null인지 아닌지 판단해서 return 해야한다.
+//		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {
+
+//			@Override
+//			public User get() {
+//				return new User(); // 빈 객체를 user에 넣는다. = null을 반환하지 않는다.
+//			}
+//		});
+		
+		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+			@Override
+			public IllegalArgumentException get() {
+				return new IllegalArgumentException("해당 유저는 없습니다. id : "+id);
+			}
+		});
+		
+//		User user = userRepository.findById(id).get();		// findById(id).get() null이 return될 리가 없어.	
+		
+//		User user = userRepository.findById(id).orElseGet(new Supplier<User>() {
+//			@Override
+//			public User get() {
+//				return new User();
+//			}
+//		});
+		
+		// 람다식 : orElseThrow() 안에 어떤 타입이 들어가야하는지 신경 안 써도 됨 익명으로 처리할 수 있다.
+//			User user = userRepository.findById(id).orElseThrow(()-> {
+//				return new IllegalArgumentException("해당 사용자는 없습니다.");
+//		});
+		
+		
+		return user;
+	}
+	
+	
 	
 	//http://localhost:8000/blog/dummy/join (요청)
 	//http의 body에  username, password, email 데이터를 가지고 요청 
